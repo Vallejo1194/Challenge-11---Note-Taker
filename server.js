d4192b8153676f7817254e0c1a54131aea4d8f1b
Vallejo1194 <adrian.vallejo94@gmail.com>
2022-10-17 22:55:33 -0500

    working on the server JS

---
+++ server.js
@@ -0,0 +1,56 @@
+
+const express = require('express');
+const fs = require('fs');
+var uniqid = require('uniqid');
+const util = require('util');
+
+
+
+const app = express();
+const PORT = 3001;
+
+let db = require('./db/db.json');
+
+app.use(express.static('public'));
+
+// Define function to write JSON file
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// HTML routes 
+
+app.get('/api/notes', (req, res) => res.json(db));
+app.get('/notes', (req, res) => {res.sendFile(__dirname + './public/notes.html')});
+
+  
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body
+
+    if (req.body ) {
+        const newNote = {
+            id: uniqid(),
+            title,
+            text
+        }
+
+        db.push(newNote);
+        writeToFile('./db/db.json',db)
+        res.json('Note Added Succesfully');     
+    
+    } else {
+        res.error('Could not add new Note');
+    }
+})
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
