0f1b213749f715177e60b59e4cbc1b0856932099
Vallejo1194 <adrian.vallejo94@gmail.com>
2022-10-29 14:54:49 -0500

    fixed the notes route

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ var uniqid = require('uniqid');
 const util = require('util');
 
 
-
+// initiate express 
 const app = express();
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3002;
 
 let db = require('./db/db.json');
 
 app.use(express.static('public'));
 
 // Define function to write JSON file
+
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
@@ -27,7 +29,8 @@ app.use(express.urlencoded({ extended: true }));
 // HTML routes 
 
 app.get('/api/notes', (req, res) => res.json(db));
-app.get('/notes', (req, res) => {res.sendFile(__dirname + './public/notes.html')});
+
+app.get('/notes', (req, res) => {res.sendFile(__dirname + '/public/notes.html')});
 
   
 app.post('/api/notes', (req, res) => {
